24cb1b8b9cfb41a2b06fd66c05031c566d934689
Sanjay Garg <sgarg5858@gmail.com>
2025-02-18 21:30:23 +0000

    2- BASIC VALIDATORS IMPLMENTATION

--- src/app/dynamic-forms/containers/dynamic-form-container/dynamic-form-container.component.ts
+++ src/app/dynamic-forms/containers/dynamic-form-container/dynamic-form-container.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject, switchMap, tap } from 'rxjs';
-import { DynamicFormConfig } from '../../interfaces/control';
-import { FormControl, FormGroup } from '@angular/forms';
+import { DynamicControl, DynamicFormConfig } from '../../interfaces/control';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form-container',
@@ -31,12 +31,30 @@ export class DynamicFormContainerComponent implements OnInit {
   private buildForm(controls: DynamicFormConfig['controls']) {
     this.form = new FormGroup({});
     Object.keys(controls).forEach((key) => {
-      this.form.addControl(key, new FormControl(controls[key].value));
+      const validators = this.resolveValidators(controls[key]);
+      this.form.addControl(key, new FormControl(controls[key].value,{validators}));
     });
+
     console.log(this.form.controls);
   }
-  submitForm()
-  {
+  resolveValidators({ validators = {}}: DynamicControl) {
+    return (Object.keys(validators) as Array<keyof typeof validators> | [] ).map((validatorKey) => {
+      const validatorValue = validators[validatorKey];
+      if (validatorKey === 'required') {
+        return Validators.required;
+      }
+      if (validatorKey === 'email') {
+        return Validators.email;
+      }
+      if (validatorKey === 'minLength') {
+        if (typeof validatorValue === 'number')
+          return Validators.minLength(validatorValue); 
+      
+      }
+      return Validators.nullValidator;
+    });
+  }
+  submitForm() {
     console.log(this.form.value);
   }
 }
--- src/app/dynamic-forms/interfaces/control.ts
+++ src/app/dynamic-forms/interfaces/control.ts
@@ -1,17 +1,25 @@
+import { ValidatorFn, Validators } from "@angular/forms";
+
 export interface DynamicOption {
   label: string;
   value: string;
 }
+type CustomValidators = {
+    banWords:ValidatorFn
+}
+export type ValidatorKeys = keyof Omit< typeof Validators & CustomValidators, "compose" | "composeAsync" | "prototype">;
+
 export interface DynamicControl<T = string> {
   controlType: 'input' | 'select';
   label: string;
   value: T | null;
   type?: string;
   options?: DynamicOption[];
+  validators?: Partial<Record<ValidatorKeys, unknown>>;
 }
-export interface DynamicFormConfig{
-    description:string;
-    controls:{
-        [key:string]: DynamicControl
-    }
+export interface DynamicFormConfig {
+  description: string;
+  controls: {
+    [key: string]: DynamicControl;
+  };
 }
