e9981228934c5cb1d9b73ca35d51ce7e63c74bcd
Sanjay Garg <sgarg5858@gmail.com>
2025-02-19 15:27:44 +0000

    Apply hostClass form-field to dynamic components

--- src/app/dynamic-forms/components/dynamic-control-base/dynamic-control-base.ts
+++ src/app/dynamic-forms/components/dynamic-control-base/dynamic-control-base.ts
@@ -1,8 +1,10 @@
-import { inject } from "@angular/core";
-import { CONTROL_DATA } from "../../control-data.token";
-import { ControlContainer, FormGroup } from "@angular/forms";
+import { Directive, Host, HostBinding, inject } from '@angular/core';
+import { CONTROL_DATA } from '../../control-data.token';
+import { ControlContainer, FormGroup } from '@angular/forms';
 
+@Directive()
 export class DynamicControlBase {
+  @HostBinding('class') class = 'form-field';
   control = inject(CONTROL_DATA);
   protected parentFormGroup = inject(ControlContainer);
   get formGroup() {
--- src/app/dynamic-forms/components/dynamic-group/dynamic-group.component.ts
+++ src/app/dynamic-forms/components/dynamic-group/dynamic-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { DynamicControlBase } from '../dynamic-control-base/dynamic-control-base';
 import { DynamicControlResolver } from '../../services/dynamic-control-resolver.service';
 
@@ -8,6 +8,6 @@ import { DynamicControlResolver } from '../../services/dynamic-control-resolver.
   styleUrls: ['./dynamic-group.component.scss'],
 })
 export class DynamicGroupComponent extends DynamicControlBase {
-  
+   @HostBinding('class') override class = 'form-field';
   dynamicControlResolver = inject(DynamicControlResolver)
 }
