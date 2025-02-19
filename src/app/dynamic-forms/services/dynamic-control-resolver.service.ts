340689628e5fbc9d236abf8ea32b786aa1e16ad9
Sanjay Garg <sgarg5858@gmail.com>
2025-02-19 17:27:06 +0000

    revert lazy loading for now

--- src/app/dynamic-forms/services/dynamic-control-resolver.service.ts
+++ src/app/dynamic-forms/services/dynamic-control-resolver.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, Type } from '@angular/core';
+import { DynamicInputComponent } from '../components/dynamic-input/dynamic-input.component';
+import { DynamicSelectComponent } from '../components/dynamic-select/dynamic-select.component';
 import { DynamicControl } from '../interfaces/control';
-import { from, Observable, of, tap } from 'rxjs';
+import { DynamicCheckboxComponent } from '../components/dynamic-checkbox/dynamic-checkbox.component';
+import { DynamicGroupComponent } from '../components/dynamic-group/dynamic-group.component';
 
 type DynamicControlMap = {
-  [T in DynamicControl['controlType']]: () => Promise<Type<any>>;
+  [T in DynamicControl['controlType']]: Type<any>;
 };
 
 @Injectable({
@@ -11,35 +14,13 @@ type DynamicControlMap = {
 })
 export class DynamicControlResolver {
   private lazyControlComponents: DynamicControlMap = {
-    input: () =>
-      import('./../components/dynamic-input/dynamic-input.component').then(
-        (c) => c.DynamicInputComponent
-      ),
-    select: () =>
-      import('./../components/dynamic-select/dynamic-select.component').then(
-        (c) => c.DynamicSelectComponent
-      ),
-    checkbox: () =>
-      import(
-        './../components/dynamic-checkbox/dynamic-checkbox.component'
-      ).then((c) => c.DynamicCheckboxComponent),
-    group: () =>
-      import('./../components/dynamic-group/dynamic-group.component').then(
-        (c) => c.DynamicGroupComponent
-      ),
+    input: DynamicInputComponent,
+    select: DynamicSelectComponent,
+    checkbox: DynamicCheckboxComponent,
+    group: DynamicGroupComponent,
   };
-
-  private loadedComponents: Map<string,Type<any>> = new Map();
-   
-
-  resolve(controlType: keyof DynamicControlMap): Observable<Type<any>> {
-    const loadedComponent = this.loadedComponents.get(controlType);
-    if (loadedComponent) {
-      return of(loadedComponent);
-    }
-    return from(this.lazyControlComponents[controlType]()).pipe(tap((component) => {
-      this.loadedComponents.set(controlType, component!);
-      return component;
-    }));
+  constructor() {}
+  resolve(controlType: keyof DynamicControlMap): Type<any> {
+    return this.lazyControlComponents[controlType];
   }
 }
